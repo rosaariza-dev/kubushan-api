43c9a4ae4e16fd8a66b101a119f17992c4dd5ec6
Rosa Ariza <techrouse2025@gmail.com>
2025-05-25 20:08:59 -0500

    feat(database): conectar base de datos MongoDB al servidor

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -7,8 +8,9 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Kubushan API");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Kubushan API is running on port ${PORT}`);
+  await connectToDatabase();
 });
 
 export default app;
