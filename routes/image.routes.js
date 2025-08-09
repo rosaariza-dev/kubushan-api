18c6a070c8604e65a218ed82243fb5258bdfd0dd
Rosa Ariza <techrouse2025@gmail.com>
2025-08-08 20:31:13 -0500

    refactor(images): eliminar endpoint de borrar imagen de Cloudinary
    - se eliminó el endpoint 'DELETE /images/:publicId' para borrar imágenes directamente en Cloudinary,  dado que la eliminación ahora se gestiona desde los controladores de productos y tipos para mantener consistencia entre el storage y la base de datos.
    - se eliminó el esquema 'deleteImageSchema'  de 'images.schema.js' dado a que ya no es necesario.

--- routes/image.routes.js
+++ routes/image.routes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import {
-  deleteImage,
   getImage,
   getImages,
 } from "../controllers/image.controller.js";
 import validateMiddleware from "../middlewares/validate.middleware.js";
 import {
-  deleteImageSchema,
   getImageSchema,
 } from "../validations/image.schema.js";
 
@@ -15,11 +13,5 @@ const imageRouter = Router();
 imageRouter.get("/", getImages);
 
 imageRouter.get("/:publicId", validateMiddleware(getImageSchema), getImage);
-
-imageRouter.delete(
-  "/:publicId",
-  validateMiddleware(deleteImageSchema),
-  deleteImage
-);
 
 export default imageRouter;
--- validations/image.schema.js
+++ validations/image.schema.js
@@ -1,4 +1,3 @@
 import { validateParamsImageSchema } from "./shared/params.validation.js";
 
 export const getImageSchema = validateParamsImageSchema;
-export const deleteImageSchema = validateParamsImageSchema;
