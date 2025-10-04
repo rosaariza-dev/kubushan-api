4ba338a02babe30a3d96ec98dff6e31a80ae2b9c
Rosa Ariza <techrouse2025@gmail.com>
2025-10-04 13:14:11 -0500

    feat(cors): configuración de cors
    - se instaló la libreria de cors.
    - se añadió la variable de entorno 'CLIENT_URL' en /config/env y .env.example.
    - se realizó la configuración de cors en app.js.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { PORT, NODE_ENV } from "./config/env.js";
+import { PORT, NODE_ENV, CLIENT_URL } from "./config/env.js";
 import connectToDatabase from "./database/mongodb.js";
 import typeRouter from "./routes/type.routes.js";
 import productRouter from "./routes/product.routes.js";
@@ -10,12 +10,21 @@ import "./config/cloudinary.js";
 import correlationMiddleware from "./middlewares/correlation.middleware.js";
 import requestLogger from "./middlewares/request-logger.middleware.js";
 import logger from "./logger/index.js";
+import emailRouter from "./routes/email.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(httpContext.middleware);
 // Correlation ID
 app.use(correlationMiddleware);
+
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    methods: ["GET"],
+  })
+);
 
 app.use(express.json());
 
@@ -26,6 +35,7 @@ app.use(requestLogger);
 app.use("/api/v1/types", typeRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/images", imageRouter);
+app.use("/api/v1/email", emailRouter);
 
 app.use(errorMiddleware);
 
--- config/env.js
+++ config/env.js
@@ -2,8 +2,11 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, 
-               NODE_ENV, 
-               DB_URI, 
-               EMAIL_PASSWORD, 
-               ACCOUNT_EMAIL } = process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  EMAIL_PASSWORD,
+  ACCOUNT_EMAIL,
+  CLIENT_URL,
+} = process.env;
