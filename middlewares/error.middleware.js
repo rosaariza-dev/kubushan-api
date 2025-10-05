04557d85bd6ca8560027b9e071f636e1d2e6ddf4
Rosa Ariza <techrouse2025@gmail.com>
2025-10-04 22:18:50 -0500

    fix(middleware): mejorar mensajes de ValidationError de MongoDB
    Cambiar Object.values(err) por Object.values(err.errors) para
    acceder correctamente a los mensajes de validación y dar contexto
    claro al usuario

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -22,7 +22,7 @@ const errorMiddleware = (err, req, res, next) => {
 
     // Mongoose validation error
     if (err.name === "ValidationError") {
-      const message = Object.values(err).map((val) => val.message);
+      const message = Object.values(err.errors).map((val) => val.message);
       error = new Error(message.join(","));
       error.statusCode = 400;
     }
